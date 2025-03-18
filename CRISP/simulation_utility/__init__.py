a9166a02e7d9350de7cca3676671c91ee4f59864
Indranil17 <indranilsaha315@gmail.com>
2025-03-18 08:36:47 +0100

    last updated the example code

--- CRISP/simulation_utility/__init__.py
+++ CRISP/simulation_utility/__init__.py
@@ -1,6 +1,7 @@
 # CRISP/simulation_utility/__init__.py
 
 from .atomic_indices import *
+from .atomic_traj_linemap import *
 from .error_analysis import *
 from .interatomic_distances import *
 from .subsampling import *
